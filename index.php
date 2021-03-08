efd9ab4ab2cf5d45791f414f91c0a6575b3a5c0d
Wilmar-Julian <wilmarsanchez905@gmail.com>
2021-03-07 19:30:40 -0500

    Primer commit: Se creo el proyecto01 con un directorio css que tiene dos archivos css y un archivo index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Proyecto Heroku</title>
+<meta charset="utf-8"/>
+<link rel = "stylesheet" constant= "text/css" href="css/normalize.css"/>
+<link rel = "stylesheet" constant= "text/css" href="css/estilos.css"/>
+</head>
+<body>
+<div id="envoltura">
+<header>
+<h1> Programaci&oacute;n Web </h1>
+</header>
+<div id="main">
+<p>En este currso aprender&aacute;s a programar p&aacute;ginas web usando git
+<?php
+
+$curso ="Construción de elementos de software2";
+echo" <br /><br />ESTAS EN EL CURSO: $curso."      ?>
+
+</p>
+
+
+
+</div>
+</div>
+
+
+</body>
+
+
+</html>
