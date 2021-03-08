cfc562b7cc1ae980ffe10da03bc93ecaafeab7a6
Wilmar-Julian <wilmarsanchez905@gmail.com>
2021-03-07 20:23:22 -0500

    Commit 2: agregando archivo pagina2.php con algo de concatenacion

---
+++ pagina2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Proyecto Heroku</title>
+<meta charset="utf-8"/>
+<link rel = "stylesheet" constant= "text/css" href="css/normalize.css"/>
+<link rel = "stylesheet" constant= "text/css" href="css/estilos.css"/>
+</head>
+
+<body>
+
+<?php 
+$a =12;
+$b = 1000;
+$a .= $b;
+
+echo "<br /> Contcatenado $a ."
+
+
+?> 
+
+
+
+</body>
+
+</html>
